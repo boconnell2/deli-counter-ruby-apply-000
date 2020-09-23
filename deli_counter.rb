6398dd83f0d057f4b75e7312fa2a8b4b07e8c3e1
Brendan O'Connell <bmoconnell15@gmail.com>
2020-09-23 22:06:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
     puts "The line is currently empty."
   else
     current_queue = "The line is currently:"
-    for i in 0..array.size do
+    for i in array.length-1 do
       current_queue += " #{i + 1}. #{array[i]}"
     end
     puts current_queue
